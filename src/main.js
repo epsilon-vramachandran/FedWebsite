bf46d132258b89f967fcd25f28febdab301cc0df
Lavanya Kodukula <lavanya.kodukula@epsilon.com>
2020-01-06 10:12:47 +0530

    team component

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import Menu from './components/shared/nav/nav';
 import Fed  from './components/fed/fed';
 import Projects from './components/projects/projects';
 import Talks from './components/talks/talks';
-import Team from './components/team/team';
+import team from './components/team/team';
 import Training from './components/training/traning';
 import Coders from './components/coders/coders';
 import Title from './components/title/title';
@@ -53,6 +53,7 @@ function Main() {
 		<Route exact path="/training" component= { Training }/>
 		<Route exact path="/coders" component= { Coders }/>
 		<Route exact path="/title" component= { Title }/>
+		<Route exact path="/team" component= { team }/>
 			</Router>
 		</React.Fragment>
   	);
